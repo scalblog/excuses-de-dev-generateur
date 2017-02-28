20ac380b92f1b2a02b8a5703b7ce746556dc7fa6
Pascal <scalblog@yahoo.fr>
2017-02-28 12:08:31 +0100

    Update excuses.js

--- excuses.js
+++ excuses.js
@@ -1,8 +1,8 @@
 function Excuse() {
 	
-	var myDog = ['chien', 'chat', 'théorie sur pythagore', 'version de Windows', 'iguane', 'navigateur pas à jour', 'avatar', 'voisin', 'complot autour de  Matrix','artefact', 'double maléfique', 'poisson rouge', 'historique dans Google'];
+	var myDog = ['chien', 'chat', 'théorie sur pythagore', 'Windows', 'iMac', 'iguane', 'navigateur pas à jour', 'avatar', 'voisin', 'complot autour de  Matrix','artefact', 'double maléfique', 'poisson rouge', 'historique dans Google'];
 	var ate = ['a écrasé', 'a remplacé', 'a effacé', 'a altéré', 'a détruit', 'a modifié irrémédiablement', 'a inversé', 'n\a pas liké', 'a recyclé', 'a tweeté sur ', 'a recalculé', 'a redéfini', 'a remis à jour', 'a abandonné', 'a refusé de plagier', 'a isolé', 'a volé', 'a trié aléatoirement', 'a renversé de l\'eau sur', 'a perdu', 'a mal interprété', 'a incinéré', 'a fait du tri sélectif sur', 'a caché', 'a reparamétré', 'a mis de côté', 'a mis sur eBay', 'a ajouté en favori sur Facebook', 'a mis sur SnapChat', 'a mis sur Youtube', 'a bu pendant que vous bossiez sur', 'a acheté sur Amazon'];
-	var myHomework = ['projet', 'navigateur', 'travail', 'session', 'Excel', 'document', 'papier', 'contrat', 'tentative de connexion', 'blog', 'article', 'carte mémoire', 'aide en ligne', 'rêve d\'un monde meilleur', 'memento', 'compte-rendu', 'presentation', 'portfolio', 'commentaire'];
+	var myHomework = ['projet', 'navigateur', 'travail', 'session', 'Excel', 'document', 'papier', 'contrat', 'tentative de connexion', 'blog', 'article', 'carte mémoire', 'aide en ligne', 'rêve d\'un monde meilleur', 'mode d\'emploi', 'compte-rendu', 'présentation', 'portfolio', 'commentaire'];
 	
 	var who = myDog[Math.round(Math.random()*(myDog.length-1))];
 	var did = ate[Math.round(Math.random()*(ate.length-1))];
